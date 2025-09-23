02f51b76180832fd371a9711dda4ee93653aa917
Kārlis Suneps <33724480+Su-Nes@users.noreply.github.com>
2025-09-23 14:24:14 +0300

    desktop microphone test and map WIP
    imported webGL mic package as well, which I'll have to switch to at some point

--- SmartStar/Assets/Common/U_Scripts/EventOnMicInput.cs
+++ SmartStar/Assets/Common/U_Scripts/EventOnMicInput.cs
@@ -6,12 +6,19 @@ using UnityEngine.Events;
 public class EventOnMicInput : MonoBehaviour
 {
     [SerializeField] private float minimumTriggerVolume = .1f;
+    [SerializeField] private float waitAfterTrigger = 1f;
+    private float t;
     
     [SerializeField] private UnityEvent volumeEvent;
     
     private void Update()
     {
-        if(FakeVoiceRecognition.MicLoudness > minimumTriggerVolume)
+        t += Time.deltaTime;
+        
+        if (FakeVoiceRecognition.MicLoudness > minimumTriggerVolume && t >= waitAfterTrigger)
+        {
             volumeEvent.Invoke();
+            t = 0f;
+        }
     }
 }
